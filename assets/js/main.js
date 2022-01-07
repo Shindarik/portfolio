b6a84d3c96bbae9c021949b15738aaa2f7c120ba
SUSLU Matéo <78606237+Shindarik@users.noreply.github.com>
2022-01-07 09:40:51 +0100

    ty

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,19 @@
+$(document).ready(function(){
 
-$(window).scroll(function(){
 
-    var scroll = $(window).scrollTop();
-    var dh = $(document).height();
-    var wh = $(window).height();
-    var scrollPourcent = (scroll / (dh - wh)) * 100;
+    $(".slide").width($(".slider").width() - 10);
+    
+    $(window).scroll(function(){
+    
+        var scroll = $(window).scrollTop();
+        var dh = $(document).height();
+        var wh = $(window).height();
+        var scrollPourcent = (scroll / (dh - wh)) * 100;
+    
+        var scrollValue = 100 - scrollPourcent;
+    
+        $(".photoMateo img").css("top", scrollValue+"px");
+    });
 
-    var scrollValue = 100 - scrollPourcent;
-
-    $(".photoMateo img").css("top", scrollValue+"px");
 });
+
