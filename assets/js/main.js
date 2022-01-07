fd36dedb0a12866356689f8372cfc37e72bd55e9
SUSLU Matéo <78606237+Shindarik@users.noreply.github.com>
2022-01-07 12:46:35 +0100

    azddaz

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,43 @@
-$(document).ready(function(){
+$(document).ready(function () {
+
+    let leftPos, movLeft = 0, movRight = 0;
+
+    setInterval(() => {
+        $(".slide").width($(".slider").width() - 10);
+    }, 10);
 
 
-    $(".slide").width($(".slider").width() - 10);
-    
-    $(window).scroll(function(){
-    
-        var scroll = $(window).scrollTop();
-        var dh = $(document).height();
-        var wh = $(window).height();
-        var scrollPourcent = (scroll / (dh - wh)) * 100;
-    
-        var scrollValue = 100 - scrollPourcent;
-    
-        $(".photoMateo img").css("top", scrollValue+"px");
+
+    $(window).scroll(function () {
+
+        let scroll = $(window).scrollTop();
+        let dh = $(document).height();
+        let wh = $(window).height();
+        let scrollPourcent = (scroll / (dh - wh)) * 100;
+
+        let scrollValue = 100 - scrollPourcent;
+
+        $(".photoMateo img").css("top", scrollValue + "px");
+    });
+
+    $(".btnLeft").on("click", function () {
+        leftPos = parseInt($(".type").css("left"));
+
+        if (leftPos < 0) {
+            movLeft += $(".slider").width();
+            movRight -= $(".slider").width();
+            $(".type").css("left", movLeft);
+        }
+    });
+
+    $(".btnRight").on("click", function () {
+        leftPos = parseInt($(".type").css("left"));
+
+        if (leftPos < $(".slider").width()) {
+            movLeft -= $(".slider").width();
+            movRight += $(".slider").width();
+            $(".type").css("left", -movRight);
+        }
     });
 
 });
-
