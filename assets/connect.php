d6fb39a247ff5bd01a404324d8bc9531015bfd46
SUSLU Matéo <78606237+Shindarik@users.noreply.github.com>
2022-02-10 10:44:57 -0500

    Update connect.php

--- assets/connect.php
+++ assets/connect.php
@@ -1,3 +1,5 @@
 <?php
     $link = new PDO('mysql:host=vi6ld.myd.infomaniak.com;dbname=vi6ld_portfolioBdd', 'vi6ld_mateossl', 'Warrior77', array (PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
 ?>
+
+//Connexion à la bdd
